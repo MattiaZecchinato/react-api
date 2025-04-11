37ebd3032f2d0ed5c9e5d3b9aab72570e7d5cbd2
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-04-11 15:26:25 +0200

    feat: milestone 1

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,26 @@
+import { useState, useEffect } from "react";
+
+// axios
+import axios from 'axios';
+
 export default function Main() {
 
+    const uri = 'https://www.freetestapi.com/api/v1/actors';
+
+    useEffect(() => {
+
+        axios.get(uri).then(res => {
+
+            const actorsList = res.data;
+            console.log(actorsList);
+        })
+        .catch(() => {
+
+            console.log('Error');
+        });
+    }, []);
+
     return <>
-        
+        {/* <button onClick={setActor}>Log actor</button> */}
     </>
 }
