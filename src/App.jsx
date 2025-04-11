f6e31ad5f63be3134e92a6ce473479a7944541f8
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-04-11 14:09:15 +0200

    feat: add Main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 // Header
 import Header from './components/Header';
 
+// Main
+import Main from './components/Main';
+
 function App() {
 
   return (
     <>
       <Header />
+      <Main />
     </>
   )
 }
